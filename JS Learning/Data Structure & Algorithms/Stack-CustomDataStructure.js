8be8e46414f25a25fa7c5ef1fa3c5d78a5a15690
Sanwal <Sanwalatwork@gmail.com>
2024-01-04 16:08:47 +0500

    init

---
+++ JS Learning/Data Structure & Algorithms/Stack-CustomDataStructure.js
@@ -0,0 +1,20 @@
+//--------------------------------------------------------|Stack|---------------------------------------------------
+
+/*
+- The stack data structure is a sequential collection of elements that follows the principle
+  of Last In First Out (LIFO).
+- The last element inserted into the stack is first element to be removed.  
+- A stack of plates. The last plate placed on top of the stack is also the first plate.
+  removed from the stack.  
+- Stack is an abstract data type. It is defined by its behavior rather than being a mathematical model.  
+  
+The Stack data structure supports two main operations:  
+- Push, which adds an element to the collection  
+- Pop, which removes the most recently added element from the collection
+
+Stack Usage:
+- Browser history tracking  
+- Undo operation when typing  
+- Expression conversions  
+- Call stack in JavaScript runtime
+*/
