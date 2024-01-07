bcc5dcc720d9e87f00e00f23cbfea10736b3b908
Sanwal <Sanwalatwork@gmail.com>
2024-01-07 16:13:59 +0500

    Diagonal Difference - ProbSolving

---
+++ JS Learning/Data Structure & Algorithms/Problem Solving/Diagonal Difference.js
@@ -0,0 +1,28 @@
+//-------------------------|Finding Diagonal Difference|------------------------------
+
+let array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+// Declaring variable to store the Diagonal values
+let sum1 = 0;
+let sum2 = 0;
+// For loop to itterate over the values to get the array of arrays diognal values
+for (let i = 0; i < array.length; i++) {
+  // Storing 1,5,9 in value1
+  let value1 = array[i][i];
+  // Storing 3,5,7 in value2
+  let value2 = array[i][array.length - i - 1];
+  console.log(value1);
+  console.log(value2);
+  // Storing the sum of 1,5,9 value1 in sum1
+  sum1 += value1;
+  // Storing the sum of 3,5,7 value2 in sum2
+  sum2 += value2;
+}
+// Absoluting the values to do not get the negative value
+let difference = Math.abs(sum1 - sum2);
+// Consoling the difference
+console.log(difference);
