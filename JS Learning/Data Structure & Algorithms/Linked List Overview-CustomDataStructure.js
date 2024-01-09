2f21c8d9f5857264023fff32f84f1ee31483064c
Sanwal <Sanwalatwork@gmail.com>
2024-01-09 14:45:44 +0500

    Linked List Overview

---
+++ JS Learning/Data Structure & Algorithms/Linked List Overview-CustomDataStructure.js
@@ -0,0 +1,18 @@
+//------------------------------------------------------|Linked List Overview|---------------------------------------------------
+
+/*
+- A linked list is a linear data structure that includes a series of connected nodes.  
+- Each node consists of a data value and a pointer that points to the next node.  
+- The list elements can be easily inserted or removed without reallocation or reorganization
+  of the entire structure.  
+- Random access of elements is not feasible and accessing an element has linear time complexity.  
+  
+The linked list data structure supports three main operations:  
+- Insertion - to add an element at the beginning, end or at a given index in the list.  
+- Deletion - to remove an item given its index or value.  
+- Search - to find an element given its value.
+
+Queue Usage:
+- All applications of both stacks and queues are applications of linked lists
+- Image viewer
+*/
