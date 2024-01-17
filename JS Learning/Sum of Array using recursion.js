64afa5878efb560581a627fb88e70f028bc2a0b8
Sanwal <Sanwalatwork@gmail.com>
2024-01-17 13:38:46 +0500

    Sum of Array using recursion

---
+++ JS Learning/Sum of Array using recursion.js
@@ -0,0 +1,16 @@
+//--------------------------------------------------------|Sum of Array using recursion|-------------------------------------------------------
+
+/*
+The base case if (arr.length === 0) returns 0 when the array is empty. This is a constant time operation.
+The recursive call sumOfArrays(arr.slice(0, arr.length - 1)) reduces the array size by one element in each
+recursive call until it reaches the base case. Each recursive call performs a constant amount of work.
+*/
+
+function sumOfArrays(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr[arr.length - 1] + sumOfArrays(arr.slice(0, arr.length - 1));
+}
+
+console.log(sumOfArrays([1, 3, 4, 5, 6]));
