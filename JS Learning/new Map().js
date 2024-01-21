ad22b0ee11beeef8c626aacf36bf0f36c2349a53
Sanwal <Sanwalatwork@gmail.com>
2024-01-21 19:10:34 +0500

    new Map()

--- JS Learning/new Map().js
+++ JS Learning/new Map().js
@@ -1,5 +1,6 @@
 //--------------------------------------------------|new Map() in JavaScript|-----------------------------------------------
 
+// Example 1
 //--- It stores key-value pairs, the duplicate values will not be appeared:
 
 let array = [[1, 2]];
@@ -26,3 +27,32 @@ mapArray.forEach((key, value) => {
 });
 
 console.log(mapArray);
+
+// Example 2
+// Creating a new map in JavaScript typically refers to using the Map object, which is a built-in data
+// structure that allows you to store key-value pairs.
+
+// Creating a new Map
+const myMap = new Map();
+
+// Adding key-value pairs to the map
+myMap.set("key1", "value1");
+myMap.set("key2", "value2");
+
+// Retrieving values from the map
+const value1 = myMap.get("key1");
+const value2 = myMap.get("key2");
+
+// Checking if a key exists in the map
+const keyExists = myMap.has("key1");
+
+// Deleting a key-value pair from the map
+myMap.delete("key1");
+
+// Clearing all key-value pairs from the map
+myMap.clear();
+
+console.log("\nMap Key-Value Pairs:");
+for (const [key, value] of myMap) {
+  console.log(`${key}: ${value}`);
+}
