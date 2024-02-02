467cab19113360543c82e4ec9637b46d694814c4
Sanwal <Sanwalatwork@gmail.com>
2024-02-02 13:23:40 +0500

    Callback function

--- JS Learning/Callback function.js
+++ JS Learning/Callback function.js
@@ -1,10 +1,24 @@
 //--------------------------------------------------|Callback function|-----------------------------------------------
 
-// --- Def: Function which are passed as an argument to another function.
+// A function passed as an argument to another function,
+// to be executed or called back at a later point in time
+// e.g, "Hey, when you're done, call this next."
 
+hello(goodbye);
+
+function hello(callback) {
+  console.log("Hello!");
+  callback();
+}
+
+function goodbye() {
+  console.log("Goodbye!");
+}
 function message() {
   console.log("Addition done successfully");
 }
+
+//----------------------------------------------------------
 
 function addition(value1, value2, callback) {
   console.log(value1 + value2);
