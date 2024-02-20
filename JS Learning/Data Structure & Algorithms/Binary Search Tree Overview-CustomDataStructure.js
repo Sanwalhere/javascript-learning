17f08cef04ff87c7a0f3dc46fb11453998fcfcb1
Sanwal <Sanwalatwork@gmail.com>
2024-02-20 11:45:20 +0500

    Binary Search Tree Overview-CustomDataStructure

---
+++ JS Learning/Data Structure & Algorithms/Binary Search Tree Overview-CustomDataStructure.js
@@ -0,0 +1,38 @@
+/*
+
+Binary Tree:
+A binary tree is a tree data structure in which each node has at most two children.
+They are referred to as left child and right child.
+
+Visualization:
+
+        A
+       / \
+      B   C
+     / \
+    D   E
+
+Binary Search Tree:
+The value of each left node must be smaller than the parent node.
+The value of each right node must be greater than the parent node.
+Each node has at most two children.
+
+Visualization:
+
+        10
+       /  \
+      5    15
+     / \
+    3   7
+
+Binary Search Tree Operation: 
+Insertion - To add a node to the tree  
+Search - To find a node given its value  
+DFS & BFS - To visit all nodes in the tree  
+Deletion - To remove a node qiven its value
+
+Binary Search Tree Usage:  
+Searching  
+Sorting  
+To implement abstract data types such as lookup tables and priority queues
+*/
