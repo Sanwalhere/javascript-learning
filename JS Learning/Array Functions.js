b9e81124fcdb3b8cb922a5546250c199c01ddc9e
Sanwal <Sanwalatwork@gmail.com>
2024-04-23 13:20:35 +0500

    Array Functions

--- JS Learning/Array Functions.js
+++ JS Learning/Array Functions.js
@@ -7,11 +7,7 @@ const isPositiveEvery = numbers.every(function (value) {
 const isPositiveSome = numbers.some(function (value) {
   return value <= 0;
 });
-const sorting = numbers.sort(function (a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
-  return 0;
-});
+
 console.log(numbers);
 console.log(isPositiveEvery);
 console.log(isPositiveSome);
